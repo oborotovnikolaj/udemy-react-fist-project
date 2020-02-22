8bf24c6c372f19d89340a65e045fd84a206f97c7
oborotovnikolaj <oborotovnikolaj@gmail.com>
2020-02-22 14:03:54 +0300

    234 Добавил state + controlled field input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,21 +3,19 @@ import React, {Component} from 'react';  //this mean pull variable React.Compone
 
 // class SearchBar extends React.Component {
 class Search_bar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {term: 'default value'};
+    }
+
     render() {
         return (
-            <div>
-                {/*<input onChange={this.onChangeInput}/>*/}
-                <input onChange={(event) => console.log(event.target.value)}/>
+            <div value={this.state.term}>
+                <input onChange={(event) => this.setState({term: event.target.value})}/>
+                {/*Current State = {this.state.term}*/}
             </div>
         );
     }
-
-    //название обычно дается из того какой элемент менятеся, в данном случае input
-    // onChangeInput(event) {
-    //     console.log(event.target.value);
-    //     // console.log(event); // можно посмотреть что из себя представляет event
-    // }
 }
-
 
 export default Search_bar;
