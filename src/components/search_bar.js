25256bf9605a1f31ef0c14f0066a0ffd188245ba
oborotovnikolaj <oborotovnikolaj@gmail.com>
2020-02-22 12:39:48 +0300

    232 Добавил простой event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,9 +6,18 @@ class Search_bar extends Component {
     render() {
         return (
             <div>
-                <input/>
-            </div>)
+                {/*<input onChange={this.onChangeInput}/>*/}
+                <input onChange={(event) => console.log(event.target.value)}/>
+            </div>
+        );
     }
+
+    //название обычно дается из того какой элемент менятеся, в данном случае input
+    // onChangeInput(event) {
+    //     console.log(event.target.value);
+    //     // console.log(event); // можно посмотреть что из себя представляет event
+    // }
 }
+
 
 export default Search_bar;
