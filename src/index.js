63093c78566d83d74e74a5dcf8623ef22b810d5d
oborotovnikolaj <oborotovnikolaj@gmail.com>
2020-02-22 14:45:03 +0300

    237 рефакторинг в класс компонент App

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import Search_bar from "./components/search_bar";
 
 const API_KEY = 'AIzaSyCwXiIPIN-XuI3WZ0BPYo5J_8zRtnrgLKQ';
-YTSearch({key: API_KEY, term: 'porno'}, function (data) {
-    console.log(data)
-});
 
-// const App = function () {   //вот эта вроде JS6
-const App = () => {     // а вот это уже Es6, разница в импользовании this
-    return (
-        <div>
-            <Search_bar/>
-        </div>
-    );
-};
+
+class App extends Component {
+    constructor(param) {
+        super(param);
+        this.state = {videos : []};
+        YTSearch({key: API_KEY, term: 'porno'}, videos => {
+            // this.setState({videos : videos})
+            this.setState({videos}) // синтаксический сахар
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Search_bar/>
+            </div>
+        );
+    };
+}
 
 // React.render(App)
 ReactDom.render(<App/>, document.querySelector('.container'));
