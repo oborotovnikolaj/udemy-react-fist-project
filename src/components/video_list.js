4e8dad35bf43e5ddd29b846916ad85d8096207a5
oborotovnikolaj <oborotovnikolaj@gmail.com>
2020-02-22 15:32:44 +0300

    239 фунция map в js и video_item. + предупреждение Warning: Each child in an array or iterator should have a unique "key" prop

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import VideoListItem from "./video_list_item";
 
 const VideoList = (props) => {
+    const VideoItems = props.videos.map((video) => {
+        // передаем проперти video значение video
+        return <VideoListItem video = {video}/>
+    });
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            {VideoItems}
         </ul>
     )
 };
+
+//loop in js in correct way
+// var array = [1,2,3]
+// array.map(function(number) {return '<div>' + number + '</div>'});
 
 export default VideoList;
---
+++ src/components/video_list_item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoListItem = (props) => {
+    return (
+        <li>
+            video
+        </li>
+    )
+};
+
+export default VideoListItem;
